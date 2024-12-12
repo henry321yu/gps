1a91217fd7a44a1bc8d5d7f144babedaa50eeee6
henry321yu <henry321yu@gmail.com>
2024-12-12 09:04:46 +0800

    Update gps_acc_log_test3.ino

--- gps_acc_log_test3/gps_acc_log_test3.ino
+++ gps_acc_log_test3/gps_acc_log_test3.ino
@@ -17,6 +17,7 @@ void setup()
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
     while (1);
   }
+  myGNSS.setI2COutput(COM_TYPE_NMEA); // 設定 I2C 為輸出 NMEA
 
   // 設定更新頻率為 20Hz
   if (myGNSS.setNavigationFrequency(5, VAL_LAYER_RAM)) // 設定更新頻率
@@ -32,23 +33,16 @@ void setup()
   byte currentRate;
   if (myGNSS.getNavigationFrequency(&currentRate))
   {
-    Serial.print("Current navigation frequency: ");
-    Serial.print(currentRate);
-    Serial.println(" Hz");
+    Serial.printf("Current navigation frequency: %d Hz \n",currentRate);
   }
   else
   {
     Serial.println("Failed to get navigation frequency!");
   }
-
-  //  myGNSS.setI2COutput(COM_TYPE_UBX); // Set the I2C port to output UBX only
-  myGNSS.setI2COutput(COM_TYPE_NMEA); // 設定 I2C 為輸出 NMEA
-//  myGNSS.setNavigationFrequency(5, VAL_LAYER_RAM);
 }
 
 void loop()
 {
-  // Query module. The module only responds when a new position or time is available
   if (myGNSS.getPVT())
   {
     uint16_t year = myGNSS.getYear();
@@ -71,15 +65,10 @@ void loop()
     float fractionalSecond = millisecond / 1000.0;
 
     double t0 = millis() * 0.001;
-//    Serial.printf("Date: %04d-%02d-%02d, Time: %02d:%02d:%06.3f\n", year, month, day, hour, minute, second + fractionalSecond);
-//    Serial.printf("Satellites: %d\n", numSats);
-//    Serial.printf("Latitude: %.7f, Longitude: %.7f\n", latitude, longitude);
-//    Serial.printf("Horizontal Accuracy: %.1f m, Vertical Accuracy: %.1f m\n", hAcc / 1000.0, vAcc / 1000.0); // Convert mm to meters
-    
-    Serial.printf("time : %.3f,Date: %04d-%02d-%02d, Time: %02d:%02d:%06.3f ,Satellites: %d ,Latitude: %.7f, Longitude: %.7f,Horizontal Accuracy: %.1f m, Vertical Accuracy: %.1f m\n",t0, year, month, day, hour, minute, second + fractionalSecond, numSats, latitude, longitude, hAcc / 1000.0, vAcc / 1000.0);
-    
+
+    Serial.printf("time : %.3f,Date: %04d-%02d-%02d, Time: %02d:%02d:%06.3f ,Satellites: %d ,Latitude: %.7f, Longitude: %.7f,Horizontal Accuracy: %.1f m, Vertical Accuracy: %.1f m\n", t0, year, month, day, hour, minute, second + fractionalSecond, numSats, latitude, longitude, hAcc / 1000.0, vAcc / 1000.0);
+
     String datedata = String(t0, 3) + "," + String(hour) + ":" +  String(minute) + ":" +  String(second + fractionalSecond, 3);
-//    Serial.printf("time,gpstime : ");
-//    Serial.println(datedata);
+    //    Serial.println(datedata);
   }
 }
